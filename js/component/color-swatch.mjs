f615deffc34a751e1464cc8802e13ee9771d2d72
Lauren Zou <zou.lauren@gmail.com>
2026-08-28 19:16:36 -0400

    Allow editing color using a color picker.

--- js/component/color-swatch.mjs
+++ js/component/color-swatch.mjs
@@ -1,52 +1,104 @@
-import { createElement } from '../utils/component.mjs';
+import { createElement, createIcon } from '../utils/component.mjs';
 import Color from '../model/color.mjs';
 import {
   AA_LARGE_TEXT_CONTRAST_RATIO,
   AA_NORMAL_TEXT_CONTRAST_RATIO,
   getContrastingColor,
 } from '../utils/contrast.mjs';
+import { getRandomNumber } from '../utils/random.mjs';
 
 export class ColorSwatchComponent extends HTMLElement {
-  connectedCallback() {
-    this.color = new Color(this.getAttribute('hex'));
+  static observedAttributes = ['hex'];
+
+  constructor() {
+    super();
+    const self = this;
+
+    // Create color name element.
+    this.colorNameElement = document.createElement('h2');
+
+    // Create values element.
+    this.hexValueElement = document.createElement('span');
+    this.rgbValueElement = document.createElement('span');
+    this.hslValueElement = document.createElement('span');
+
+    // Create edit color input and label.
+    this.editColorInputElement = Object.assign(
+      document.createElement('input'),
+      { type: 'color' },
+    );
+    this.editColorInputElement.addEventListener('input', (event) => {
+      self.setAttribute('hex', event.target.value);
+    });
+    this.editColorButtonElement = document.createElement('label');
+    this.editColorButtonElement.appendChild(createIcon('edit'));
+  }
+
+  attributeChangedCallback(attributeName, _oldValue, newValue) {
+    if (attributeName !== 'hex') return;
+
+    // Set bg and fg colors.
+    const color = new Color(newValue);
     const normalTextColor = getContrastingColor(
-      this.color.hex,
+      color.hex,
       AA_NORMAL_TEXT_CONTRAST_RATIO,
     );
     const largeTextColor = getContrastingColor(
-      this.color.hex,
+      color.hex,
       AA_LARGE_TEXT_CONTRAST_RATIO,
     );
-    this.style.setProperty('--swatch-color', this.color.hex);
+    this.style.setProperty('--swatch-color', color.hex);
     this.style.setProperty('--selection-bg-color', normalTextColor);
     this.style.color = normalTextColor;
 
-    const nameElement = createElement('h2', this.color.name);
-    nameElement.style.color = largeTextColor;
-    this.appendChild(nameElement);
+    // Set color name.
+    this.colorNameElement.textContent = color.name;
+    this.colorNameElement.style.color = largeTextColor;
 
-    // Add hex, rgb, and hsl values.
-    const valuesContainer = document.createElement('div');
-    valuesContainer.className = 'values-container';
-    this.appendChild(valuesContainer);
+    // Add hex, RGB, and HSL values.
+    this.hexValueElement.textContent = color.hex;
+    this.rgbValueElement.textContent = `${color.rgb.r}, ${color.rgb.g}, ${color.rgb.b}`;
+    this.hslValueElement.textContent = `${color.hsl.h}, ${color.hsl.s}, ${color.hsl.l}`;
 
-    valuesContainer.appendChild(createElement('label', 'hex'));
-    valuesContainer.appendChild(createElement('span', this.color.hex));
+    // Set color on edit color action.
+    const editColorId = `edit-color-${color.hex}-${getRandomNumber(0, 100)}`;
+    this.editColorInputElement.id = editColorId;
+    this.editColorInputElement.value = color.hex;
+    this.editColorButtonElement.setAttribute('for', editColorId);
+  }
 
-    valuesContainer.appendChild(createElement('label', 'rgb'));
+  connectedCallback() {
+    // Set up edit color action.
+    const editColorContainer = Object.assign(document.createElement('div'), {
+      className: 'edit-color-action',
+    });
+    editColorContainer.append(
+      this.editColorInputElement,
+      this.editColorButtonElement,
+    );
+
+    // Set up actions container.
+    const actionsContainer = Object.assign(document.createElement('div'), {
+      className: 'actions-container',
+    });
+    actionsContainer.append(editColorContainer);
+
+    // Set up values container.
+    const valuesContainer = Object.assign(document.createElement('div'), {
+      className: 'values-container',
+    });
     valuesContainer.append(
-      createElement(
-        'span',
-        `${this.color.rgb.r}, ${this.color.rgb.g}, ${this.color.rgb.b}`,
-      ),
+      createElement('label', 'hex'),
+      this.hexValueElement,
+
+      createElement('label', 'rgb'),
+      this.rgbValueElement,
+
+      createElement('label', 'hsl'),
+      this.hslValueElement,
     );
 
-    valuesContainer.appendChild(createElement('label', 'hsl'));
-    valuesContainer.appendChild(
-      createElement(
-        'span',
-        `${this.color.hsl.h}, ${this.color.hsl.s}, ${this.color.hsl.l}`,
-      ),
-    );
+    // Append elements to component.
+    this.append(actionsContainer, this.colorNameElement, valuesContainer);
   }
 }
--- js/utils/component.mjs
+++ js/utils/component.mjs
@@ -1,9 +1,21 @@
 /**
  * @param {string} tagName
  * @param {string} textContent
+ * @returns {HTMLElement}
  */
 export function createElement(tagName, textContent) {
   const element = document.createElement(tagName);
   element.textContent = textContent;
+  return element;
+}
+
+/**
+ * @param {string} iconName
+ * @returns {HTMLSpanElement}
+ */
+export function createIcon(iconName) {
+  const element = document.createElement('span');
+  element.className = 'material-symbols-outlined';
+  element.textContent = iconName;
   return element;
 }
